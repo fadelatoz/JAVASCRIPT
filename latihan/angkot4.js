7bf38cdd1e8537e35530d0abd75e37f25dec2046
fadelatoz <66379727+fadelatoz@users.noreply.github.com>
2020-06-03 23:55:29 +0700

    Update angkot4.js

--- latihan/angkot4.js
+++ latihan/angkot4.js
@@ -1,15 +1,20 @@
 no_angkot = 1;
 jml_angkot = 10;
-angkot_oprasi= 6
+angkot_oprasi = 6;
 
-for(no_angkot;no_angkot <= angkot_oprasi;no_angkot++){
-    if(no_angkot <= angkot_oprasi){
-        console.log('angkot no' + no_angkot +' ' + 'sedang beroprasi');
-    }else if (no_angkot == 8){
-        console.log('angkot no ' + no_angkot +' ' + 'sedang tidak beroprasi');
-    }else if(no_angkot == 7){
-        console.log('angkot no'+ no_angkot + ' sedang tidak beroprasi');
-    }else{
-        console.log('angkot no '+ no_angkot +' ' + 'sedang tidak beroprasi')
+for(no_angkot;no_angkot <=10; no_angkot++){
+    if(no_angkot <=6 ){
+        console.log('angkot no ' + no_angkot +' ' +'sedang beroprasi');
     }
-}
+    else if(no_angkot == 8){
+        console.log('angkot no '+ no_angkot + ' ' + 'sedang lembur');
+    }
+    else if ( no_angkot == 7){
+        console.log('angkot no ' + no_angkot +' ' + 'sedang tidak beroprasi')
+    }
+    else {
+        console.log('angkot no ' + no_angkot +' ' + 'sedang tidak beroprasi')
+    }
+
+        
+    
